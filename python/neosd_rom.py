39ce4a76b9de3347479947316a9d4ef3ed3e4a28
fliperdeboteco <fliperdeboteco@outlook.com>
2024-01-06 01:08:29 -0300

    Python neosd/.neo to darksoft scripts

---
+++ python/neosd_rom.py
@@ -0,0 +1,48 @@
+import os
+import struct
+from binascii import crc32
+
+class NeoSDRom:
+  def __init__(self, file_path):
+    self.file_path = file_path
+    self.file_content = open(file_path, "rb").read()
+    self.header_size = 4096
+    self.sizes = struct.unpack("<IIIIII", self.file_content[4:28])
+    self.eproms = self.get_eproms()
+
+  def get_eproms(self):
+    eproms = {}
+    offset = self.header_size
+    for index, name in enumerate(['prom', 'srom', 'm1rom', 'vroma0', 'vroma2', 'crom0']):
+      size = self.sizes[index]
+      eproms[name] = self.file_content[offset:offset+size]
+      if name == 'crom0':
+        crom0 = bytearray(eproms[name])
+        crom0[1::4], crom0[2::4] = crom0[2::4], crom0[1::4] # 17027A61 for kof97
+        eproms[name] = crom0
+      offset += size
+    return eproms
+
+  def get_prom(self):
+    return self.eproms['prom']
+
+  def get_srom(self):
+    return self.eproms['srom']
+
+  def get_m1rom(self):
+    return self.eproms['m1rom']
+
+  def get_vroma0(self):
+    return self.eproms['vroma0']
+
+  def get_crom0(self):
+    return self.eproms['crom0']
+
+  def get_file_path(self):
+    return self.file_path
+
+  def get_file_name(self):
+    return os.path.basename(self.file_path)
+
+  def get_file_name_without_extension(self):
+    return os.path.splitext(self.get_file_name())[0]
---
+++ python/neosd_to_darksoft.py
@@ -0,0 +1,33 @@
+import sys
+import os
+from os import path
+from binascii import crc32
+from neosd_rom import NeoSDRom
+
+if len(sys.argv) < 2:
+  print("No input given")
+  exit()
+
+if len(sys.argv) < 3:
+  print("No output given")
+  exit()
+
+neo_file_path = sys.argv[1]
+
+print(f"Reading {neo_file_path}")
+
+neofile = NeoSDRom(neo_file_path)
+output_dir = path.join(sys.argv[2], neofile.get_file_name_without_extension())
+
+if not path.exists(output_dir):
+  os.mkdir(output_dir)
+
+print(f"Writing to {output_dir}")
+
+for key in neofile.eproms:
+  eprom_path = path.join(output_dir, key)
+  print(f"{key}\tsize: {len(neofile.eproms[key])} \tcrc: {hex(crc32(neofile.eproms[key]))}")
+  with open(eprom_path, "wb") as f:
+    f.write(neofile.eproms[key])
+  
+print("Done")
