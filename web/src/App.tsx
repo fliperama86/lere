515c1769a63724355a479bc8d5544eea367f6b15
Eduardo Cobuci <educobuci@gmail.com>
2025-03-19 23:02:04 -0300

    Core package

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,16 @@
 import { Button } from '@/components/ui/button'
 import { ThemeProvider } from '@/components/theme-provider'
+import { lere } from '@lere/core'
 
 function App() {
+  const onClick = () => {
+    console.log(lere)
+  }
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <div className="h-dvh flex flex-col items-center justify-center gap-4 w-full">
         <h1 className="text-3xl font-bold underline">Hello world!</h1>
-        <Button>Click me</Button>
+        <Button onClick={onClick}>Click me</Button>
       </div>
     </ThemeProvider>
   )
